9e6f4cbbc2e5a101d5dadc76408be15975d3a4c9
taozebi <taozebi@qq.com>
2017-02-21 14:33:06 +0800

    modify userList

--- source/lsbc/WebContent/js/controller/system/userListController.js
+++ source/lsbc/WebContent/js/controller/system/userListController.js
@@ -10,4 +10,45 @@ app.controller("userListController",['$scope','request','dialog',function($scope
 			});
 		}
 	});
+	$scope.modify = function(){
+		alert('modify userInfo');
+		/*$scope.modifyUser = {};
+		request.modify('/userInfo/updateUserInfo.action',$scope.modifyUser,function(data){
+			if(data.status == '0'){
+				添加成果
+				dialog.info({
+					items : {title:'提示信息',content:'修改用户信息成功!',type:'success',success:function(data){
+						alert('success');
+					}}
+				});
+			}else{
+				dialog.info({
+					items : {title:'获取用户信息失败',content:'请检查服务器配置!',type:'warn'}
+				});
+			}
+		});*/
+	};
+	$scope.del = new　function(){
+		alert('delete user');
+		/*dialog.info({
+			items : {title:'提示信息',content:'确定删除该用户?',type:'success',success:function(data){
+				alert('success');
+				$scope.delUser = {};
+				request.modify('/userInfo/delUserInfo.action',$scope.delUser,function(data){
+					if(data.status == '0'){
+						添加成果
+						dialog.info({
+							items : {title:'提示信息',content:'删除用户成功!',type:'success',success:function(data){
+								alert('success');
+							}}
+						});
+					}else{
+						dialog.info({
+							items : {title:'删除用户失败',content:'请检查服务器配置!',type:'warn'}
+						});
+					}
+				});
+			}}
+		});*/
+	}; 
 }]);
