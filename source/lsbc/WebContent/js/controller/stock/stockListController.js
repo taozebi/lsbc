bcd19267b1403e965eac895a5f8b9907c4b750a7
taozebi <taozebi@qq.com>
2017-02-22 09:41:01 +0800

    modify stockList query

--- source/lsbc/WebContent/js/controller/stock/stockListController.js
+++ source/lsbc/WebContent/js/controller/stock/stockListController.js
@@ -1,13 +1,27 @@
 app.controller("stockListController",['$scope','request','dialog',function($scope,request,dialog){
 	$scope.goodsList = new Array();
-	request.get('/goods/getGoodsList.action',{},function(data){
-		if(data.status == '0'){
-			//data是一个list集合
-			$scope.goodsList = data.data;
-		}else{
-			dialog.info({
-				items : {title:'获取商品信息失败',content:'请检查服务器配置!',type:'warn'}
-			});
-		}
-	});
+	$scope.search = {
+			goodsName : '',
+			goodsTypeId : ''
+	};
+	$scope.findStockList = function(){
+		request.get('/goods/getGoodsList.action',$scope.search,function(data){
+			if(data.status == '0'){
+				//data是一个list集合
+				$scope.goodsList = data.data;
+			}else{
+				dialog.info({
+					items : {title:'获取商品信息失败',content:'请检查服务器配置!',type:'warn'}
+				});
+			}
+		});
+	};
+	$scope.searchChange = function(){
+		$scope.findStockList();
+	};
+	$scope.resetSearch = function(){
+		$scope.search = {goodsName : '',goodsTypeId : ''};
+		$scope.findStockList();
+	};
+	$scope.findStockList();
 }]);
